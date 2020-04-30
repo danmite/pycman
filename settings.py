5441115e035fc8bf4a375c1d8b6395f738c3c1b3
doodmaster <doodmaster@doods-MacBook-Pro-8.local>
2020-04-30 17:36:15 +0200

    init pycman

---
+++ settings.py
@@ -0,0 +1,26 @@
+from pygame.math import Vector2 as vec
+
+# screen settings
+WIDTH, HEIGHT = 610, 670
+FPS = 60
+TOP_BOTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT-TOP_BOTTOM_BUFFER
+
+ROWS = 30
+COLS = 28
+
+# color settings
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+RED = (208,22,22)
+GREY = (107, 107, 107)
+PLAYER_COLOR = (190, 194, 15)
+
+# font settings
+START_TEXT_SIZE = 30
+START_FONT = 'arial black'
+
+# player settings
+# PLAYER_START_POS = vec(1, 1)
+
+# mob settings
